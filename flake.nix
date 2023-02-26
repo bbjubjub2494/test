f302c24f24b494f6fa965c2b8bbffe01b8965a19
Louis Bettens <louis@bettens.info>
2023-02-26 15:38:02 +0100

    fmt

--- flake.nix
+++ flake.nix
@@ -3,29 +3,42 @@
 
   inputs.hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
-  inputs.hercules-ci-effects.flakeModule
+        inputs.hercules-ci-effects.flakeModule
       ];
 
-      systems = [ "x86_64-linux" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
         packages.default = pkgs.hello;
+
+        formatter = pkgs.alejandra;
       };
-      
-hercules-ci.flake-update.enable = true;
+
+      hercules-ci.flake-update.enable = true;
+      hercules-ci.flake-update.when = "daily";
 
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-
       };
     };
 }
